37b3c6ea20fcec034a05b6b8ad175b337faabea7
Adam Farley <farley13@gmail.com>
2020-12-19 19:49:34 -0500

    removing default footer

--- pages/index.js
+++ pages/index.js
@@ -20,14 +20,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <div>Don&#39;t Worry!</div>
       </footer>
     </div>
   )
